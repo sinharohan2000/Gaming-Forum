cbc93739778973c4460108f81b12a15f85104590
mayanksingh31094 <singhviru31094@gmail.com>
2020-10-30 13:41:55 +0530

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -51,12 +51,12 @@ Route::post('/changeprofile',[UserController::class,'changeprofile'])->middlewar
 Route::get('/redirect', [GoogleLoginController::class,'redirect']);
 Route::get('/callback', [GoogleLoginController::class,'callback']);
 Route::get('/updateusername',function(){
-	return view('user.updateusername');
+	return view('user.updateusername')->middleware('authorize');
 })->middleware('authorize');
 Route::post('/updateusername',[UserController::class,'updateusername'])->middleware('authorize');
 Route::post('/avail',[UserController::class,'avail']);
-Route::get('/chat/{receiver}', [ChatController::class,'chat']);
-Route::get('/chats', [ChatController::class,'chats']);
+Route::get('/chat/{receiver}', [ChatController::class,'chat'])->middleware('authorize');
+Route::get('/chats', [ChatController::class,'chats'])->middleware('authorize');
 Route::fallback(function () {
     return view('error');
 });
